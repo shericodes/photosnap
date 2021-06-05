9d7cdd91d04d622c6472ccbdfaabec51c0c0baf0
Sheri Richardson <shrdson@gmail.com>
2021-06-05 19:31:28 +0200

    Add reset and global styles

---
+++ web/src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/reset.css";
+import "../../styles/global.css";
+import "../../styles/typography.css";
+
+const Layout = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,10 +1,8 @@
-import * as React from "react"
+import * as React from "react";
 
 // markup
 const IndexPage = () => {
-  return (
-    <h1>Photosnap</h1>
-  )
-}
+  return <></>;
+};
 
-export default IndexPage
+export default IndexPage;
