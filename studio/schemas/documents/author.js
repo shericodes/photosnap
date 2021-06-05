4da492acaef12d9d59459abe90bd3dfb6d411259
Sheriallis Richardson <shrdson@gmail.com>
2021-06-05 16:26:31 +0200

    Sanity schemas (#1)
    * Add Sanity schemas
    
    * Import cta

---
+++ studio/schemas/documents/author.js
@@ -0,0 +1,31 @@
+export default {
+  name: "author",
+  title: "Author",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "text",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 100,
+      },
+    },
+  ],
+};
---
+++ studio/schemas/objects/cta.js
@@ -0,0 +1,22 @@
+export default {
+  name: "cta",
+  title: "Call To Action",
+  type: "object",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "link",
+      title: "Link",
+      type: "string",
+    },
+    {
+      name: "linkLabel",
+      title: "Link Label",
+      type: "string",
+    },
+  ],
+};
